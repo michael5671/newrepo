f8f21c81a530f3ef2cc9910365e393d5fbbdfeac
michael5671 <22521102@gm.uit.edu.vn>
2024-04-17 02:12:13 +0700

    chuyen file

---
+++ test.js
@@ -0,0 +1,17 @@
+var diem = "G";
+switch (diem) {
+    case "Y":
+        document.write("Yếu");
+        break;
+    case "TB":
+        document.write("Trung bình");
+        break;
+    case "K":
+        document.write("Khá");
+        break;
+    case "G":
+        document.write("Giỏi");
+        break;
+    default:
+        document.write("Xuất sắc");
+}
